aaa87c150d4179b17e148fb042933e5a29b9a108
cfiander <cfiander90@gmail.com>
2019-05-31 09:19:41 -0700

    addfolder functioning

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
 import NoteListMain from '../NoteListMain/NoteListMain';
 import NotePageMain from '../NotePageMain/NotePageMain';
+import AddFolder from '../AddFolder/AddFolder';
 import ApiContext from '../ApiContext';
 import config from '../config';
 import './App.css';
@@ -42,6 +43,12 @@ class App extends Component {
         });
     };
 
+    handleAddFolder = folder => {
+        this.setState({
+            folders: [...this.state.folders, folder],
+        })
+    }
+
     renderNavRoutes() {
         return (
             <>
@@ -55,6 +62,7 @@ class App extends Component {
                 ))}
                 <Route path="/note/:noteId" component={NotePageNav} />
                 <Route path="/add-folder" component={NotePageNav} />
+                <Route path ="/add-folder" component={AddFolder}/>
                 <Route path="/add-note" component={NotePageNav} />
             </>
         );
@@ -80,7 +88,8 @@ class App extends Component {
         const value = {
             notes: this.state.notes,
             folders: this.state.folders,
-            deleteNote: this.handleDeleteNote
+            deleteNote: this.handleDeleteNote,
+            addFolder: this.handleAddFolder
         };
         return (
             <ApiContext.Provider value={value}>
--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -19,6 +19,7 @@ export default class NotePageMain extends React.Component {
   render() {
     const { notes=[] } = this.context
     const { noteId } = this.props.match.params
+    console.log(this.props)
     const note = findNote(notes, noteId) || { content: '' }
     return (
       <section className='NotePageMain'>
